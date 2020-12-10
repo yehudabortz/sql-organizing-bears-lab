4552b322476b7ddb1b91d644568be96e09201ae5
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-10 13:12:40 -0600

    commit tree 7f83b56a37d856555d962bc2600ed75eebead906

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Grump", 24, "M", "Brown", "Friendly", 1),
+(2, "Chew", 14, "M", "Black", "Mean", 1),
+(3, "Chloe", 16, "F", "White", "Agressive", 0),
+(4, "DuDe", 12, "M", "Brown", "Friendly", 1),
+(5, "Ted", 8, "M", "Grey", "Shy", 1),
+(6, "Teddy", 4, "F", "White", "Friendly", 1),
+(7, "Nick", 24, "M", "Brown", "Friendly", 1),
+(8, null, 24, "F", "Black", "Scared", 1);
